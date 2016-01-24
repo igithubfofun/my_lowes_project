9c74232d1d01f88bfc134b96fa2d1d3a77f4d2ed
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2016-01-23 18:34:25 -0600

    fixed register to add token to database

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var User = require('../models/user');
 var Project = require('../models/project')
 var mongoose = require('mongoose');
 var request = require('request');
-
+var token = "";
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
@@ -20,11 +20,43 @@ router.get('/my_projects', function(req, res){
   });
 });
 
+/* Login form */
+router.get('/login', function(req, res){
+  res.render('login');
+});
+
+router.post('/authenticate', function(req, res){
+  var logonId = req.body.logonId;
+  var logonPassword = req.body.logonPassword;
+  var lowes_logon = {
+    logonId: logonId,
+    logonPassword: logonPassword
+  }
+  var key = process.env.LOWES_API_KEY;
+  var url = 'http://api.lowes.com/customer/login?api_key='+key;
+  request({
+          url: url,
+          method: "POST",
+          headers: {
+            'Authorization':'Basic QWRvYmU6ZW9pdWV3ZjA5ZmV3bw=='
+          },
+          json: true,   // <--Very important!!!
+          body: lowes_logon
+      }, function (error, response, body){
+          console.log('body: ',body);
+          console.log(body.SSOToken);
+          token = body.SSOToken;
+          console.log('token: ',token);
+          console.log('response: ',response);
+          console.log('error: ',error);
+          res.redirect('/my_projects', {email: logonId});
+      });
+
+});
+
 /* New User form */
 router.get('/register', function(req, res){
-  var key = process.env.LOWES_API_KEY;
-  var url = "http://api.lowes.com/customer/registration?api_key="+key;
-	res.render('register', {fail: "hi", key: key});
+	res.render('register');
 });
 
 router.post('/new_user', function(req,res,next){
@@ -35,22 +67,24 @@ router.post('/new_user', function(req,res,next){
 	var zipCode = req.body.zipCode;
 	var address1 = req.body.address1;
 	var address2 = req.body.address2;
+  var city = req.body.city;
 	var state = req.body.state;
 	var firstName = req.body.firstName;
 	var lastName = req.body.lastName;
 
-	var new_user = new User({
-    email: email,
-    password1: password1,
-    password2: password2, 
-    phone: phone, 
-    zipCode: zipCode, 
-    address1: address1, 
-    address2: address2, 
-    state: state, 
-    firstName: firstName, 
-    lastName: lastName
-  });
+	// var new_user = new User({
+ //    email: email,
+ //    password1: password1,
+ //    password2: password2, 
+ //    phone: phone, 
+ //    zipCode: zipCode, 
+ //    address1: address1, 
+ //    address2: address2,
+ //    city: city, 
+ //    state: state, 
+ //    firstName: firstName, 
+ //    lastName: lastName
+ //  });
 
 var lowes_user = {
     phoneUS: phone,
@@ -63,7 +97,7 @@ var lowes_user = {
     lastName: lastName,
     activityGuid: "",
     myLowesCardNumber: "",
-    city: "Austin",
+    city: city,
     state: state,  
     address1: address1, 
     address2: address2,
@@ -72,49 +106,73 @@ var lowes_user = {
 var key = process.env.LOWES_API_KEY;
 var url = 'http://api.lowes.com/customer/registration?api_key='+key;
 
-// request({
-//     url: url,
-//     method: "POST",
-//     json: true,   // <--Very important!!!
-//     body: lowes_user
-// }, function (error, response, body){
-//     console.log(response);
-// });
-// $.ajax({
-//   url: url,
-//   dataType: "json",
-//   method: "POST",
-//   data: lowes_user,
-//   success: function(data, textStatus, jqXHR){
-//     res.json(data);
-//   },
-//   error: function(XMLHttpRequest, textStatus, errorThrown){
-//       alert("Status: " + textStatus); alert("Error: " + errorThrown);
-//   }
-// });
-
-  new_user.save(function(err) {
-    if (err) {
-      res.redirect('/register', {fail: err});
+request({
+      url: url,
+      method: "POST",
+      headers: {
+        'Authorization':'Basic QWRvYmU6ZW9pdWV3ZjA5ZmV3bw=='
+      },
+      json: true,   // <--Very important!!!
+      body: lowes_user
+    }, function (error, response, body){
+    if (error) {
+      console.log('error: ',error);
+      res.redirect('/register');
     } else {
-      console.log('User saved successfully');
-      request({
-          url: url,
-          method: "POST",
-          headers: {
-            'Authorization':'Basic QWRvYmU6ZW9pdWV3ZjA5ZmV3bw=='
-          },
-          json: true,   // <--Very important!!!
-          body: lowes_user
-      }, function (error, response, body){
-          console.log('body: ',body);
-          console.log('response: ',response);
-          console.log('error: ',error);
+      console.log('body: ',body);
+      console.log(body.SSOToken)
+      token = body.SSOToken;
+      console.log('response: ',response);
+      var new_user = new User({
+        email: email,
+        password1: password1,
+        password2: password2, 
+        phone: phone, 
+        zipCode: zipCode, 
+        address1: address1, 
+        address2: address2,
+        city: city, 
+        state: state, 
+        firstName: firstName, 
+        lastName: lastName,
+        token: token
       });
-      res.redirect('/');
+      new_user.save(function(err) {
+        if (err) {
+          console.log('User did not save');
+        } else {
+          console.log('User saved successfully');
+          res.redirect('/');
+        }
+      });
     }
-  }); 
+  });
 });
+
+//   new_user.save(function(err) {
+//     if (err) {
+//       res.redirect('/register', {fail: err});
+//     } else {
+//       console.log('User saved successfully');
+//       request({
+//           url: url,
+//           method: "POST",
+//           headers: {
+//             'Authorization':'Basic QWRvYmU6ZW9pdWV3ZjA5ZmV3bw=='
+//           },
+//           json: true,   // <--Very important!!!
+//           body: lowes_user
+//       }, function (error, response, body){
+//           console.log('body: ',body);
+//           console.log(body.SSOToken)
+//           token = body.SSOToken;
+//           console.log('response: ',response);
+//           console.log('error: ',error);
+//       });
+//       res.redirect('/');
+//     }
+//   }); 
+// });
 
 module.exports = router;
 
